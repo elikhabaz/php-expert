a0692dc9772528af6b461ba518adc930d19d9c12
elikhabaz <elahe.khabaz.bavani@gmail.com>
2024-12-02 23:28:27 +0330

    new

---
+++ 05-array/index.php
@@ -0,0 +1,24 @@
+<?php
+    ///arrays
+    $x = array();
+    $x = array("ali" , "eli");
+    var_dump($x);
+    echo "<br>";
+
+    //newes
+    $x = [];
+    $x = ["ali" , "eli"];
+    var_dump($x);
+    echo "<br>";
+
+    $cc = array("iran" => "tehran" , "england" => "london");
+    var_dump($cc);
+    echo "<br>";
+    var_dump($cc["iran"]);
+
+
+
+
+
+
+?>
